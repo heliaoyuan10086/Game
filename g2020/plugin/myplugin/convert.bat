936623426c984edbd406796c2a751085759330a8
vin120 <vin_120@163.com>
2019-09-18 20:00:17 +0800

    engine2 map

---
+++ g2020/plugin/myplugin/convert.bat
@@ -0,0 +1,3 @@
+@rem ..\..\..\..\..\dev\tools\lualibs\lua\lua53.exe ..\..\..\..\tools\json2csv.lua %1
+@..\..\..\..\..\dev\tools\lualibs\lua\lua53.exe ..\..\..\..\tools\JsonCsvConverter.lua %1
+@pause
